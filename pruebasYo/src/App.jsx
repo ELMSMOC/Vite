bc7efad3ff094df160ecb1415c3ea7e7bd33b6c2
Eugenio Lazaro <eugeniolazaro.elm@gmai>
2026-04-27 13:15:09 +0200

    haciendo pruebas

--- pruebasYo/src/App.jsx
+++ pruebasYo/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
-import styled, { css } from styled-components
-
 import './App.css'
 
+import styled, { css } from styled-components
+import ejerciciosdata from './data/rutinasyo'
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  const ejdiario = ejerciciosdata[0];
+
+  console.log(ejerciciosdata);
   return (
     <h1>Hola</h1>
+
+
   )
 }
 
---
+++ pruebasYo/src/data/rutinasyo.js
@@ -0,0 +1,15 @@
+
+import img1 from "../assets/imgs/laterales.png"
+import img1 from "../assets/imgs/puente.png"
+import img1 from "../assets/imgs/sentadillas.png"
+
+export default [
+    {
+        "titulo":"Dia 1",
+        "rutina" : [
+            {"ejercicio":"Laterales", "img": img1, "reps": "15"},
+            {"ejercicio":"Puente", "img": img2, "reps": "12"},
+            {"ejercicio":"Sentadillas", "img": img3, "reps": "10"},
+        ]
+    }
+];
