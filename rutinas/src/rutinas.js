a6d918a550918fda1d31022ffa03c17fc3fd6b45
Eugenio Lazaro <eugeniolazaro.elm@gmai>
2026-05-12 12:56:20 +0200

    testng

--- rutinas/src/rutinas.js
+++ rutinas/src/rutinas.js
@@ -1,3 +1,26 @@
-import img1 from './assets/imgs/1.jpg';
-import img2 from './assets/imgs/2.jpg';
-import img3 from './assets/imgs/3.jpg';
+import sentadillas from './assets/imgs/sentadillas.png';
+import puente from './assets/imgs/puente.png';
+import laterales from './assets/imgs/3.jpg';
+
+export default [
+    {
+       "titulo": "Rutina dia 1",
+       "rutina": [
+          {
+             "ejercicio": "Sentadillas",
+             "img": sentadillas,
+             "repeticiones": 10
+          },
+          {
+             "ejercicio": "Laterales",
+             "img": laterales,
+             "repeticiones": 9
+          },
+          {
+             "ejercicio": "Puente",
+             "img": puente,
+             "repeticiones": 5
+          },
+       ]
+    }
+]
---
+++ testing1/src/rutinas.js
@@ -0,0 +1,26 @@
+import sentadillas from './assets/sentadillas.png';
+import puente from './assets/puente.png';
+import laterales from './assets/3.jpg';
+
+export default[
+    { 
+         "titulo": "Rutina dia 1",
+         "rutina":[
+            {
+               "ejercicio": "Sentadillas",
+               "img": sentadillas,
+               "repeticiones": 5
+            },
+            {
+               "ejercicio": "Laterales",
+               "img": laterales,
+               "repeticiones": 9
+            },
+            {
+               "ejercicio": "Puente",
+               "img": puente,
+               "repeticiones": 5
+            }
+         ]
+     } 
+]
