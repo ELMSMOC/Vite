072e39a7696f4bf83d75c8d2dd51ab3a3ef98323
Eugenio Lazaro <eugeniolazaro.elm@gmai>
2026-05-19 18:50:16 +0200

    Aventura

--- Aventura/src/App.jsx
+++ Aventura/src/App.jsx
@@ -3,18 +3,41 @@ import './App.css'
 
 import styled, {css} from 'styled-components'
 import  escenas from './assets/json/mijson'
-import { Escena } from './styled'
+import { Escena, ZonaPlaya } from './styled'
 
 
 function App() {
   
-const obtenerEscenaPorNombre = title => escenas.find(obj => obj.title == title)
+const obtenerEscenaPorNombre = titulo => escenas.find(obj => obj.titulo == titulo)
 
 const [escenaActual, setEscenaActual] = useState(obtenerEscenaPorNombre("principal"))
+
+const [ancho, setWidth] = useState(0);
+const [alto, setHeight] = useState(0);
+
+const calculaDimensionEscena = () => {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+  const a= 16;
+  const b= 9;
+
+  const proporcion = a / b;
+  const proporcionPantalla = w / h;
+
+  if (proporcion>proporcionPantalla) {
+    setWidth(w);
+    setHeight(w / proporcion); // Calcula el alto que corresponde al ancho dado la proporción 16/9
+  } else {
+    setHeight(h);
+    setWidth(h * proporcion); // Calcula el ancho que corresponde al alto dado la proporción 16/9
+  }
+}
 
 return (
   <Escena fondo={escenaActual.fondo}>
     {escenaActual.descripcion}
+
+    <ZonaPlaya></ZonaPlaya>
   </Escena>
 )
 
--- Aventura/src/styled.js
+++ Aventura/src/styled.js
@@ -1,10 +1,35 @@
 import styled, { css } from 'styled-components';
 
+const cssComun = css`
+    position: absolute;
+    top: (${props => props.y}px);
+    left: (${props => props.x}px);
+    width: (${props => props.width}px);
+    height: (${props => props.height}px);
+    background-color: rgba(195, 69, 69, 0.5);
+    display: block;
+    cursor: pointer;
+    &:hover {
+        background-color: rgba(255, 255, 255, 0.8);
+    }
+`
+
 export const Escena= styled.div`
   height: 100vh;
-  width: 100vw;
+  margin: 0;
+  padding: 0;
+  width: 100vw ;
+  max-width: 100% !important;
   background-color: black;
   background-image: url(${props => props.fondo});
   background-size: cover;
   background-position: center;
 `
+
+export const ZonaPlaya = styled.div`
+    ${cssComun}
+`;
+
+export const ZonaMontana = styled.div`
+    ${cssComun}
+`;
